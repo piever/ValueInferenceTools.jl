429efc7e501e7f3d12d57a4d5194fd0637d1b098
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-05 21:34:13 +0000

    Files generated by PkgTemplates

---
+++ src/ValueInferenceTools.jl
@@ -0,0 +1,5 @@
+module ValueInferenceTools
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ValueInferenceTools
+using Test
+
+@testset "ValueInferenceTools.jl" begin
+    # Write your own tests here.
+end
