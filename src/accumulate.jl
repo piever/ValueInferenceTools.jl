71274afba4d0689d2158f5d571e4507a32f4046e
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-05 23:16:33 +0000

    Fixed probability computation

--- src/accumulate.jl
+++ src/accumulate.jl
@@ -1,12 +1,44 @@
 to_sign(b::Bool) = ifelse(b, 1, -1)
 
-function computevalue(rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}, γ; start = 0.0)
+function to_diff(s::T) where T
+    x = 1-1/(1+s)
+    x - (1 - x)
+end
+
+function valuediff(rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}, γ::T) where T
+    start = zero(T)
     N = length(rewards)
     @assert N == length(sides)
     vals = fill(start, N)
     for i in 1:N
         previous = i == 1 ? start : vals[i-1] 
-        vals[i] = (1-γ) * previous + γ * rewards[i] * to_sign(side[i]) 
+        vals[i] = (one(T)-γ) * previous + γ * rewards[i] * to_sign(sides[i]) 
+    end
+    vals
+end
+
+function valuediff(rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}, prob::TaskStats{T}) where T
+    ratios = probabilityratio(rewards, sides, prob)
+    to_diff.(ratios) .* prob.rwd
+end
+
+function probabilityratio(rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}, prob::TaskStats{T}) where T
+    start = one(T)
+    N = length(rewards)
+    vals = fill(start, N)
+    reward_evidence = zero(T)
+    failure_evidence = one(T) / (one(T) - prob.rwd)
+    for i = 1:N
+        prior = if i == 1
+            start
+        else
+            prevlow = sides[i-1] ? one(T) / vals[i-1] : vals[i-1]
+            lowprior = (prevlow + prob.dpl) / (one(T) - prob.dpl)
+            (sides[i-1] == sides[i]) ? lowprior : one(T) / lowprior
+        end
+        evidence = rewards[i] ? reward_evidence : failure_evidence
+        posterior = prior * evidence
+        vals[i] = sides[i] ? one(T) / posterior : posterior
     end
     vals
 end
