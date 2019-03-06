258c848687febc1dbddd9966802a5806f73cd1aa
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-06 14:08:48 +0000

    inference struct

--- src/accumulate.jl
+++ src/accumulate.jl
@@ -22,28 +22,48 @@ end
 
 ################################################################################
 
-function valuediff(rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}, prob::TaskStats{T}) where T
-    ratios = probabilityratio(rewards, sides, prob)
+function valuediff(prob::TaskStats{T}, rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}) where T
+    ratios = probabilityratio(prob, rewards, sides)
     to_diff.(ratios) .* prob.rwd
 end
 
-function probabilityratio(rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}, prob::TaskStats{T}) where T
-    start = one(T)
+mutable struct InferenceAccumulator{T}
+    prob::TaskStats{T}
+    value::T
+    side::Union{Bool, Missing}
+    reward_evidence::T
+    failure_evidence::T
+    function InferenceAccumulator(prob::TaskStats{T}, value::T = zero(T), side::Union{Bool, Missing} = missing;
+                                  reward_evidence::T = zero(T),
+                                  failure_evidence::T = one(T) / (one(T) - prob.rwd)) where {T}
+        new{T}(prob, value, side, reward_evidence, failure_evidence)
+    end
+end
+
+Base.getindex(s::InferenceAccumulator) = s.value
+Base.setindex!(s::InferenceAccumulator, val) = (s.value = val; s.value)
+
+function updateprobabilityratio!(acc::InferenceAccumulator{T}, rwd, sd)::T where T
+    prob = acc.prob
+    prior = if ismissing(acc.side)
+        acc[]
+    else
+        prevlow = acc.side ? one(T) / acc[] : acc[]
+        lowprior = (prevlow + prob.dpl) / (one(T) - prob.dpl)
+        (acc.side == sd) ? lowprior : one(T) / lowprior
+    end
+    evidence = ifelse(rwd, acc.reward_evidence, acc.failure_evidence)
+    posterior = prior * evidence
+    acc.side = sd
+    setindex!(acc, sd ? one(T) / posterior : posterior)
+end
+
+function probabilityratio(prob::TaskStats{T}, rewards::AbstractVector{Bool}, sides::AbstractVector{Bool}) where T
+    acc = InferenceAccumulator(prob)
     N = length(rewards)
-    vals = fill(start, N)
-    reward_evidence = zero(T)
-    failure_evidence = one(T) / (one(T) - prob.rwd)
-    for i = 1:N
-        prior = if i == 1
-            start
-        else
-            prevlow = sides[i-1] ? one(T) / vals[i-1] : vals[i-1]
-            lowprior = (prevlow + prob.dpl) / (one(T) - prob.dpl)
-            (sides[i-1] == sides[i]) ? lowprior : one(T) / lowprior
-        end
-        evidence = rewards[i] ? reward_evidence : failure_evidence
-        posterior = prior * evidence
-        vals[i] = sides[i] ? one(T) / posterior : posterior
+    vals = Vector{T}(undef, N)
+    for (i, (rw, sd)) in enumerate(zip(rewards, sides))
+        vals[i] = updateprobabilityratio!(acc, rw, sd)
     end
     vals
 end
